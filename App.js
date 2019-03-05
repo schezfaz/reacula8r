d5d54fa9229bb5c3acbe5f0f109119c086522b2c
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2019-03-05 23:31:45 +0530

    updated node_modules, cretaed boiler plate

--- App.js
+++ App.js
@@ -1,50 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, { Component } from "react"
+import {StyleSheet, Text, View} from "react-native"
+import styles from "./styles"
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component{
+  constructor(){
+    super()
+    this.state = {}
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
+  render(){
+    return(
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.result}></View>
+        <View style={styles.calculation}></View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}></View>
+          <View style={styles.operators}></View>
+        </View>
       </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}
---
+++ styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1
+    },
+
+    result:{
+        flex:1,
+        backgroundColor: "red",
+    },
+
+    calculation:{
+        flex:1
+    },
+
+    buttons:{
+        flex:1
+    },
+
+    numbers:{
+        flex:1
+    },
+
+    operators:{
+        flex:1
+    }
+
+
+
+
+
+});
+
+export default styles;
